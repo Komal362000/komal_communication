5d338793c89bb943a9b85cd6cd7167ffcb43dca0
essam SROUR <essam.srour@valeo.com>
2026-01-23 03:10:27 +0200

    Reslove GenericSkeleton review feedback, fix layering violations

--- score/mw/com/impl/skeleton_base.h
+++ score/mw/com/impl/skeleton_base.h
@@ -70,7 +70,7 @@ class SkeletonBase
      * \api
      * \brief Offer the respective service to other applications
      * \return On failure, returns an error code according to the SW Component Requirements SCR-17434118 and
-     *         SCR-566325.
+     * SCR-566325.
      */
     [[nodiscard]] ResultBlank OfferService() noexcept;
 
@@ -99,6 +99,9 @@ class SkeletonBase
     SkeletonBase(SkeletonBase&& other) noexcept;
     SkeletonBase& operator=(SkeletonBase&& other) noexcept;
 
+    
+    FlagOwner service_offered_flag_;
+
   private:
     std::unique_ptr<SkeletonBinding> binding_;
     SkeletonEvents events_;
@@ -110,8 +113,6 @@ class SkeletonBase
 
     [[nodiscard]] score::ResultBlank OfferServiceEvents() const noexcept;
     [[nodiscard]] score::ResultBlank OfferServiceFields() const noexcept;
-
-    FlagOwner service_offered_flag_;
 };
 
 class SkeletonBaseView
